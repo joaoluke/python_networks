0b8af4f252f151c39e47eee59da2e76af131d436
john_luke_wavecode <joao@wavecode.com.br>
2021-03-19 22:05:42 -0300

    feat: updating mensage to server UDP

--- client_UDP/main.py
+++ client_UDP/main.py
@@ -5,7 +5,7 @@ target_port = 8000
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.bind((target_host, target_port))
-client.sendto('AAABBBCCC'.encode('utf-8'), (target_host, target_port))
+client.sendto('Hello, Server UDP'.encode('utf-8'), (target_host, target_port))
 
 data, addr = client.recvfrom(4096)
 

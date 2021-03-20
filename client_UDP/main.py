cdb257d4bba2a2ff183c7cd977a55d07cea2186e
john_luke_wavecode <joao@wavecode.com.br>
2021-03-19 21:47:21 -0300

    feat: client UDP added

---
+++ client_UDP/main.py
@@ -0,0 +1,12 @@
+import socket
+
+target_host = '127.0.0.1'
+target_port = 8000
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.bind((target_host, target_port))
+client.sendto('AAABBBCCC'.encode('utf-8'), (target_host, target_port))
+
+data, addr = client.recvfrom(4096)
+
+print(data, addr)
